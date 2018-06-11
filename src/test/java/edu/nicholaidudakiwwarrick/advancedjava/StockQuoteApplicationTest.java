56b61f94cbfa518bf20471cae2adb5fc1019231c
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2018-06-11 00:11:18 -0500

    Add basic main method to test StockQuote creation using StockService interface

--- src/test/java/edu/nicholaidudakiwwarrick/advancedjava/StockQuoteApplicationTest.java
+++ src/test/java/edu/nicholaidudakiwwarrick/advancedjava/StockQuoteApplicationTest.java
@@ -1,4 +1,27 @@
 package edu.nicholaidudakiwwarrick.advancedjava;
 
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test for the Main method
+ */
 public class StockQuoteApplicationTest {
+    private String[] args;
+
+    @Before
+    public void setup() {
+        args = new String[]{"GOOG", "2018-06-08 00:00:00", "2018-06-15 00:00:00"};
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void testMainNegative() {
+        StockQuoteApplication.main(null);
+    }
+
+    @Test
+    public void testMainPositive() {
+        StockQuoteApplication.main(args);
+    }
 }
