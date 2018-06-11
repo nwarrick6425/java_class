3ec28d44adc4af12338d72ec4f301b1ccad80e0f
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2018-06-11 00:13:41 -0500

    Added static service factory to create BasicStockService instances

--- src/main/java/edu/nicholaidudakiwwarrick/advancedjava/StockServiceFactory.java
+++ src/main/java/edu/nicholaidudakiwwarrick/advancedjava/StockServiceFactory.java
@@ -1,11 +1,23 @@
 package edu.nicholaidudakiwwarrick.advancedjava;
 
+/**
+ * Factory to create instances of <code>BasicStockService</code>
+ *
+ * @author Nicholai Dudakiw-Warrick
+ */
 public final class StockServiceFactory {
 
+    /**
+     * Private constructor to prevent instantiation of static factory instances
+     */
     private StockServiceFactory() {
     }
 
-    public static final StockService newInstance(String symbol) {
+    /**
+     * @return the <code>BasicStockService</code> instance as a reference to
+     *          the <code>StockService</code> interface
+     */
+    public static final StockService newInstance() {
         return new BasicStockService();
     }
 }
