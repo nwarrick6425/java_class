73e6550b3ee4a4976f5a4272403cf6d76d7a99e9
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2018-06-24 21:58:55 -0500

    Added database class files, utilities, and exception classes

---
+++ src/main/java/edu/nicholaidudakiwwarrick/advancedjava/DatabaseConnectionException.java
@@ -0,0 +1,4 @@
+package edu.nicholaidudakiwwarrick.advancedjava;
+
+public class DatabaseConnectionException {
+}
--- src/main/java/edu/nicholaidudakiwwarrick/advancedjava/app/StockQuoteApplication.java
+++ src/main/java/edu/nicholaidudakiwwarrick/advancedjava/app/StockQuoteApplication.java
@@ -16,26 +16,41 @@ public class StockQuoteApplication {
      */
     public static void main(@NotNull String[] args) {
 
+        StockService stockService = null;
+
         // create a StockService instance using the StockServiceFactory static factory method
-        StockService stockService = StockServiceFactory.newInstance();
+        try {
+            stockService = StockServiceFactory.getInstance(ServiceType.BASIC);
+        } catch (StockServiceException e) {
+            e.getMessage();
+        }
 
         // Retrieve date formatter and parse the 2nd and 3rd date args
         DateTimeFormatter formatter = StockQuote.getDateFormatter();
         DateTime startDate = DateTime.parse(args[1], formatter);
         DateTime endDate = DateTime.parse(args[2], formatter);
 
-        // Print a single stock quote to the console
-        System.out.println("Print a single stock quote");
-        System.out.println(stockService.getQuote(args[0], startDate).toString());
-
-        // Print multiple stock quotes that fall between both date ranges
-        System.out.println("Print multiple stock quotes for the given date range");
-        List<StockQuote> stockList = stockService.getQuote(args[0], startDate, endDate);
-
-        for (StockQuote quote : stockList) {
-            System.out.println(quote.toString());
+        System.out.println("Basic Stock Service Quotes");
+        System.out.println("****************************************");
+        try {
+            // Print a single stock quote to the console
+            System.out.println("Print a single stock quote");
+            System.out.println(stockService.getQuote(args[0], startDate).toString());
+        } catch (StockServiceException e) {
+            e.getMessage();
         }
 
+        try {
+            // Print multiple stock quotes that fall between both date ranges
+            System.out.println("Print multiple stock quotes for the given date range");
+            List<StockQuote> stockList = stockService.getQuote(args[0], startDate, endDate);
+
+            for (StockQuote quote : stockList) {
+                System.out.println(quote.toString());
+            }
+        } catch (StockServiceException e) {
+            e.getMessage();
+        }
         // Print multiple stock quotes between the period at the given interval
         System.out.println("Print multiple stock quotes for the given date range at the given interval");
         if(args[3].toUpperCase().equals("HOURLY")) {
@@ -60,6 +75,26 @@ public class StockQuoteApplication {
                 System.out.println(quote.toString());
             }
         }
+
+        System.out.println("Database Stock Service Quotes");
+        System.out.println("*********************************************");
+
+        // create a StockService instance using the StockServiceFactory static factory method
+        try {
+            stockService = StockServiceFactory.getInstance(ServiceType.DATABASE);
+        } catch (StockServiceException e) {
+            e.getMessage();
+        }
+
+        try {
+            // Print a single stock quote to the console from the database
+            System.out.println("Print a single stock quote");
+            System.out.println(stockService.getQuote(args[0], startDate).toString());
+        } catch (StockServiceException e) {
+            e.getMessage();
+        }
+
+
 
 
 
