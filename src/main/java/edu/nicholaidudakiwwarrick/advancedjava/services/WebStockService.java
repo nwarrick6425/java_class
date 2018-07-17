2249b38fb764e919368190bfd791af0fca32ef36
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2018-07-17 00:31:27 -0500

    Added StockService implementation for a web service api with testing - need to add interval getQuote method

--- src/main/java/edu/nicholaidudakiwwarrick/advancedjava/services/WebStockService.java
+++ src/main/java/edu/nicholaidudakiwwarrick/advancedjava/services/WebStockService.java
@@ -1,7 +1,107 @@
 package edu.nicholaidudakiwwarrick.advancedjava.services;
 
+import edu.nicholaidudakiwwarrick.advancedjava.model.StockQuote;
+import edu.nicholaidudakiwwarrick.advancedjava.util.IntervalEnum;
+import org.joda.time.DateTime;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
 /**
+ * Implementation of the {@code StockService} interface that obtains stock quotes from a web service
+ *
  * @author Nicholai Dudakiw-Warrick
  */
-public class WebStockService {
+public class WebStockService implements StockService {
+
+    // public fields of this class - returns an instance of the WebStockService
+    public static final WebStockService INSTANCE = new WebStockService();
+
+    /**
+     * Hidden constructor - delegates instance creation to {@code ServiceFactory}
+     */
+    protected WebStockService(){
+        // empty body
+    }
+
+    /**
+     * @param symbol the stock symbol of the company you want a quote for.
+     *               e.g. APPL for APPLE
+     * @param date the date of the stock quote needed
+     * @return a {@code StockQuote} that contains information pulled from the Yahoo Finance API
+     * @throws StockServiceException when there is error in connecting to the Yahoo Finance API or
+     *                               if the passed symbol is not found
+     */
+    public final StockQuote getQuote(String symbol, DateTime date) throws StockServiceException {
+        StockQuote stockQuote = null;
+        Calendar calDate = new DateTime(date).toCalendar(Locale.getDefault());
+        try {
+            Stock stock = YahooFinance.get(symbol, calDate);
+            stockQuote = new StockQuote(stock.getQuote().getSymbol(),
+                    stock.getQuote().getPrice(),
+                    new DateTime(stock.getQuote().getLastTradeTime()));
+        } catch (IOException e) {
+            throw new StockServiceException(e.getMessage(), e);
+        }
+        if (stockQuote == null)
+            throw new StockServiceException("No instance of " + symbol + " found in the selected range");
+
+        return stockQuote;
+    }
+
+    /**
+     * Returns a list of {@code StockQuote} objects for the passed symbol between the start and end date.
+     * Quotes are returned for each day by default.
+     *
+     * @param symbol the stock symbol to search for
+     * @param start the start date of the historical {@code StockQuote} data
+     * @param end the end date of the historical {@code StockQuote} data
+     * @return a list of {@code StockQuote} objects
+     * @throws StockServiceException when there is an IO connection exception or if the list is empty
+     */
+    public final List<StockQuote> getQuote(String symbol, DateTime start, DateTime end) throws StockServiceException {
+        List<StockQuote> stockQuotes = new ArrayList<StockQuote>();
+        try {
+            Stock stock = YahooFinance.get(symbol);
+            List<HistoricalQuote> historicalQuoteList = stock.getHistory(start.toGregorianCalendar(),
+                    end.toGregorianCalendar(),
+                    Interval.DAILY);
+            stockQuotes = quoteListAdapter(historicalQuoteList);
+        } catch (IOException e) {
+            throw new StockServiceException(e.getMessage(), e);
+        }
+        if (stockQuotes == null)
+            throw new StockServiceException("No instance of " + symbol + " found in the selected range");
+
+        return stockQuotes;
+    }
+
+    public final List<StockQuote> getQuote(String symbol, DateTime start, DateTime end, IntervalEnum interval) throws StockServiceException {
+        // TO-DO - Add stock quote functionality using intervals
+        return new ArrayList<>();
+    }
+
+    /**
+     * Private helper method to convert a {@code HistoricalQuote} list to a {@code StockQuote} list
+     *
+     * @param quotes {@code HistoricalQuote} list to be converted
+     * @return a list of {@code StockQuote} objects
+     */
+    private final List<StockQuote> quoteListAdapter(List<HistoricalQuote> quotes) {
+        List<StockQuote> stockQuotes = new ArrayList<>();
+        for (HistoricalQuote quote : quotes) {
+            stockQuotes.add(new StockQuote(quote.getSymbol(), quote.getAdjClose(), new DateTime(quote.getDate())));
+        }
+        return stockQuotes;
+    }
+
 }
--- src/test/java/edu/nicholaidudakiwwarrick/advancedjava/services/WebStockServiceTest.java
+++ src/test/java/edu/nicholaidudakiwwarrick/advancedjava/services/WebStockServiceTest.java
@@ -1,7 +1,119 @@
 package edu.nicholaidudakiwwarrick.advancedjava.services;
 
+import edu.nicholaidudakiwwarrick.advancedjava.model.StockQuote;
+import edu.nicholaidudakiwwarrick.advancedjava.util.IntervalEnum;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
+ * JUnit tests for the {@code WebStockService} class
+ *
  * @author Nicholai Dudakiw-Warrick
  */
 public class WebStockServiceTest {
+
+    private StockService service;
+    private static final String symbol = "AAPL";
+    private static DateTime start;
+    private static DateTime end;
+    private static final int NUMBER_OF_DAYS = 100;
+    private static IntervalEnum interval;
+
+    /**
+     * Sets up common logic for all tests
+     *
+     * @throws StockServiceException if an incorrect {@code ServiceType} is passed to the {@code ServiceFactory}
+     */
+    @Before
+    public void setup() throws StockServiceException {
+        service = ServiceFactory.getStockServiceInstance(ServiceType.WEB);
+        start = DateTime.now().minusDays(NUMBER_OF_DAYS).withTimeAtStartOfDay();
+        end = DateTime.now().withTimeAtStartOfDay();
+        interval = IntervalEnum.DAILY;
+    }
+
+    /**
+     * Tests that the stock symbol retrieved from getQuote is the same as what was passed
+     * @throws StockServiceException if there is an IO exception retrieving the {@code StockQuote} or if the
+     *                               the {@code StockQuote} is null
+     */
+    @Test
+    public void testGetQuoteDoubleArgStockSymbolPositive() throws StockServiceException {
+        assertTrue("Symbol returned from getQuote is not the same that was passed",
+                service.getQuote(symbol, end).getSymbol().equals(symbol));
+    }
+
+    /**
+     * Tests that the stock symbol returned is not something other than what was passed
+     * @throws StockServiceException if there is an IO exception retrieving the {@code StockQuote} or if the
+     *                               the {@code StockQuote} is null
+     */
+    @Test
+    public void testGetQuoteDoubleArgStockSymbolNegative() throws StockServiceException {
+        assertFalse("Symbol returned is the same as the invalid symbol",
+                service.getQuote(symbol, end).getSymbol().equals("BURG"));
+    }
+
+    /**
+     * Tests that the {@code DateTime} returned is not after the date passed
+     * @throws StockServiceException if there is an IO exception retrieving the {@code StockQuote} or if the
+     *                               the {@code StockQuote} is null
+     */
+    @Test
+    public void testGetQuoteDoubleArgDatePositive() throws StockServiceException {
+        assertTrue("The date returned is after the passed date",
+                !service.getQuote(symbol, end).getDate().isAfterNow());
+    }
+
+    /**
+     * Test that the {@code DateTime} returned is not the same as a future date
+     * @throws StockServiceException if there is an IO exception retrieving the {@code StockQuote} or if the
+     *                               the {@code StockQuote} is null
+     */
+    @Test
+    public void testGetQuoteDoubleArgDateNegative() throws StockServiceException {
+        assertFalse("The date returned is the same as a future time",
+                service.getQuote(symbol, end).getDate().getMillis() == (DateTime.now().getMillis() + 2600));
+    }
+
+    @Test
+    public final void testGetQuoteTripleArgStockSymbolPositive() throws StockServiceException {
+        List<StockQuote> qList = service.getQuote(symbol, start, end);
+        for (StockQuote q : qList) {
+            assertTrue("Symbol returned from getQuote is not same as symbol passed as argument", q.getSymbol().equals(symbol));
+        }
+    }
+
+    @Test
+    public final void testGetQuoteTripleArgStockSymbolNegative() throws StockServiceException {
+        List<StockQuote> qList = service.getQuote(symbol, start, end);
+        for (StockQuote q : qList) {
+            assertFalse("Symbol returned from getQuote is the same as an invalid symbol",
+                    q.getSymbol().equals("BERG"));
+        }
+    }
+
+    @Test
+    public final void testGetQuoteTripleArgTimePositive() throws StockServiceException {
+        List<StockQuote> qList = service.getQuote(symbol, start, end);
+        for (StockQuote q : qList) {
+            assertTrue("Date returned from getQuote is not within parameter range",
+                    !q.getDate().isBefore(start) || !q.getDate().isAfter(end));
+        }
+    }
+
+    @Test
+    public final void testGetQuoteTripleArgTimeNegative() throws StockServiceException {
+        List<StockQuote> qList = service.getQuote(symbol, start, end);
+        for (StockQuote q : qList) {
+            assertFalse("Date returned from getQuote is not within parameter range",
+                    q.getDate().isBefore(start) || q.getDate().isAfter(end));
+        }
+    }
 }
