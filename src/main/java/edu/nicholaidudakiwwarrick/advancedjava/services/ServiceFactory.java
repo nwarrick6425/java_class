5494c2a0848ccca2b54b33501308107c76cb7228
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2018-07-17 00:26:51 -0500

    Added WebStockService to StockService factory

--- src/main/java/edu/nicholaidudakiwwarrick/advancedjava/services/ServiceFactory.java
+++ src/main/java/edu/nicholaidudakiwwarrick/advancedjava/services/ServiceFactory.java
@@ -19,13 +19,17 @@ public final class ServiceFactory {
     /**
      * @param type {@code ServiceType} enum that specifies the type of {@code StockService} to return
      * @return the {@code BasicStockService} or {@code DatabaseStockService} instance as a reference to
-     * the {@code StockService} interface
+     *              the {@code StockService} interface
+     * @throws StockServiceException when an invalid service type is passed - be sure to use the types listed
+     *                               in the {@code ServiceType} enumeration
      */
     public static final StockService getStockServiceInstance(ServiceType type) throws StockServiceException {
         if (type.equals(ServiceType.BASIC)) {
             return new BasicStockService();
         } else if (type.equals(ServiceType.DATABASE)) {
             return new DatabaseStockService();
+        } else if (type.equals(ServiceType.WEB)) {
+            return new WebStockService();
         } else {
             throw new StockServiceException("Error: Invalid ServiceType");
         }
